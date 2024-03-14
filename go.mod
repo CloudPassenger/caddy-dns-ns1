2532689015ea55ae3ed63fb54cac63aebc8c8db6
CloudPassenger <coliwatoshinzo@gmail.com>
2024-03-14 13:10:53 +0000

    :sparkles: feat: Implementing the NS1 API

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/caddy-dns/template
+module github.com/CloudPassenger/caddy-dns-ns1
 
 go 1.16
 
 require (
-	github.com/caddyserver/caddy/v2 v2.4.1
-	github.com/libdns/template v0.1.0
+	github.com/CloudPassenger/libdns-ns1 v0.0.0-20240314130853-3458920e4123 // indirect
+	github.com/caddyserver/caddy/v2 v2.7.6
 )
--- ns1.go
+++ ns1.go
@@ -1,15 +1,15 @@
-package template
+package ns1
 
 import (
-	"fmt"
+	"time"
 
+	ns1 "github.com/CloudPassenger/libdns-ns1"
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
-	libdnstemplate "github.com/libdns/template"
 )
 
 // Provider lets Caddy read and manipulate DNS records hosted by this DNS provider.
-type Provider struct{ *libdnstemplate.Provider }
+type Provider struct{ *ns1.Provider }
 
 func init() {
 	caddy.RegisterModule(Provider{})
@@ -18,42 +18,52 @@ func init() {
 // CaddyModule returns the Caddy module information.
 func (Provider) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "dns.providers.template",
-		New: func() caddy.Module { return &Provider{new(libdnstemplate.Provider)} },
+		ID: "dns.providers.ns1",
+		New: func() caddy.Module {
+			return &Provider{new(ns1.Provider)}
+		},
 	}
 }
 
-// TODO: This is just an example. Useful to allow env variable placeholders; update accordingly.
 // Provision sets up the module. Implements caddy.Provisioner.
+
 func (p *Provider) Provision(ctx caddy.Context) error {
-	p.Provider.APIToken = caddy.NewReplacer().ReplaceAll(p.Provider.APIToken, "")
-	return fmt.Errorf("TODO: not implemented")
+	repl := caddy.NewReplacer()
+	p.Provider.APIKey = repl.ReplaceAll(p.Provider.APIKey, "")
+	return nil
 }
 
-// TODO: This is just an example. Update accordingly.
 // UnmarshalCaddyfile sets up the DNS provider from Caddyfile tokens. Syntax:
 //
-// providername [<api_token>] {
-//     api_token <api_token>
-// }
+//	providername [<api_token>] {
+//	    api_token <api_token>
+//	}
 //
 // **THIS IS JUST AN EXAMPLE AND NEEDS TO BE CUSTOMIZED.**
 func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 	for d.Next() {
 		if d.NextArg() {
-			p.Provider.APIToken = d.Val()
+			p.Provider.APIKey = d.Val()
 		}
 		if d.NextArg() {
 			return d.ArgErr()
 		}
 		for nesting := d.Nesting(); d.NextBlock(nesting); {
 			switch d.Val() {
-			case "api_token":
-				if p.Provider.APIToken != "" {
-					return d.Err("API token already set")
+			case "api_key":
+				if d.NextArg() {
+					p.Provider.APIKey = d.Val()
 				}
 				if d.NextArg() {
-					p.Provider.APIToken = d.Val()
+					return d.ArgErr()
+				}
+			case "timeout":
+				if d.NextArg() {
+					timeout, error := time.ParseDuration(d.Val())
+					if error != nil {
+						return error
+					}
+					p.Provider.Timeout = timeout
 				}
 				if d.NextArg() {
 					return d.ArgErr()
@@ -63,8 +73,8 @@ func (p *Provider) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
 			}
 		}
 	}
-	if p.Provider.APIToken == "" {
-		return d.Err("missing API token")
+	if p.Provider.APIKey == "" {
+		return d.Err("missing API key")
 	}
 	return nil
 }
